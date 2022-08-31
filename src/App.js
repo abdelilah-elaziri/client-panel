365c6dc7fd7300cdcb1b596877f031d3e0804b2f
abdelilah <abdelilahelaziri96@gmail.com>
2022-08-31 22:55:32 +0100

    create component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
+import Contact from './components/contacts/Contact';
 
 function App() {
+
+  let name = "abdelilah Elaziri";
+  let num1 = 11;
+  let num2 = 100;
+  
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <h1>Hello React.Js First Code</h1>
+      
+      <Contact/>
+      <Contact/>
+      
     </div>
   );
 }
---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+         <h1>Contacts</h1>
+         <ul>
+            <li>Name: abdelilah ELAZIRI</li>
+            <li>Email: abdelilahelaziri96@gmail.com</li>
+         </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
